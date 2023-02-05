3a0051f71fcb1d3eb233870a506077d7ae98ae10
Joshua <olajidejoshua4real@gmail.com>
2023-02-05 04:57:20 +0100

    Update auth controller with DisableOTP method to disable OTP

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,162 +6,192 @@ import { resCall } from "../helpers/resCall";
 import { Prisma } from "@prisma/client";
 import { totp, generateSecret } from "speakeasy";
 
-const RegisterUser = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { name, email, password } = req.body;
+const RegisterUser = catchAsync(async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
 
-      await prisma.user.create({
-        data: {
-          name,
-          email,
-          password: crypto.createHash("sha256").update(password).digest("hex"),
-        },
-      });
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: crypto.createHash("sha256").update(password).digest("hex"),
+      },
+    });
 
-      resCall(
-        { status: "success", message: "Registered successfully, Please login" },
-        200
-      );
-    } catch (error) {
-      if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        if (error.code === "P2002") {
-          return resCall(
-            {
-              status: "fail",
-              message: "Email already exist, Please user another email address",
-            },
-            409
-          );
-        }
-
-        return resCall({ status: "error", message: error.message }, 500);
-      }
-    }
-  }
-);
-
-const LoginUser = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { email, password } = req.body;
-
-      const user = await prisma.user.findUnique({ where: { email } });
-
-      if (!user) {
+    resCall(
+      { status: "success", message: "Registered successfully, Please login" },
+      200
+    );
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
         return resCall(
-          { status: "fail", message: "No user with that email exists" },
-          404
+          {
+            status: "fail",
+            message: "Email already exist, Please user another email address",
+          },
+          409
         );
       }
-      const userRes = {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        otp_enabled: user.otp_enabled,
-      };
-      resCall({ status: "success", userRes }, 200);
-    } catch (err) {}
+
+      return resCall({ status: "error", message: error.message }, 500);
+    }
   }
-);
+});
 
-const GenerateOTP = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { userid } = req.body;
-      const {} = generateSecret({
-        issuer: "http://localhost:8000",
-        name: "admin@admin.com",
-        length: 15,
-      });
+const LoginUser = catchAsync(async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
 
-      await prisma.user.update({
-        where: { id: userid },
-        data: {
-          otp_ascii: ascii,
-          otp_auth_url: otpauthurl,
-          otp_base32: base32,
-          otp_hex: hex,
-        },
-      });
+    const user = await prisma.user.findUnique({ where: { email } });
 
-      resCall(res, { base32, otpauthurl }, 200);
-    } catch (err) {}
-  }
-);
-
-const VerifyOTP = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { userid, token } = req.body;
-      const user = await prisma.user.findUnique({ where: { id: userid } });
-      const message = "Token is invalid or user doesn't exist";
-      if (!user) {
-        return resCall({ status: "fail", message }, 401);
-      }
-
-      const verified = totp.verify({
-        secret: user.otp_base32!,
-        encoding: "base32",
-        token,
-      });
-
-      if (!verified) {
-        return resCall({ status: "fail", message }, 401);
-      }
-
-      const updatedUser = await prisma.user.update({
-        where: { id: userid },
-        data: {
-          otp_enabled: true,
-          otp_verified: true,
-        },
-      });
-
-      resCall(
-        {
-          otp_verified: true,
-          user: {
-            id: updatedUser.id,
-            name: updatedUser.name,
-            email: updatedUser.email,
-            otp_enabled: updatedUser.otp_enabled,
-          },
-        },
-        200
+    if (!user) {
+      return resCall(
+        { status: "fail", message: "No user with that email exists" },
+        404
       );
-    } catch (err) {
-      resCall({ status: "error", message: err.message }, 500);
     }
-  }
-);
+    const userRes = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      otp_enabled: user.otp_enabled,
+    };
+    resCall({ status: "success", userRes }, 200);
+  } catch (err) {}
+});
 
-const ValidateOTP = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { userid, token } = req.body;
-      const user = await prisma.user.findUnique({ where: { id: userid } });
+const GenerateOTP = catchAsync(async (req: Request, res: Response) => {
+  try {
+    const { userid } = req.body;
+    const {} = generateSecret({
+      issuer: "http://localhost:8000",
+      name: "admin@admin.com",
+      length: 15,
+    });
 
-      const message = "Token is invalid or user doesn't exist";
-      if (!user) {
-        return resCall({ status: "fail", message }, 401);
-      }
+    await prisma.user.update({
+      where: { id: userid },
+      data: {
+        otp_ascii: ascii,
+        otp_auth_url: otpauthurl,
+        otp_base32: base32,
+        otp_hex: hex,
+      },
+    });
 
-      const validToken = totp.verify({
-        secret: user?.otp_base32 as string,
-        token,
-        window: 1,
-      });
+    resCall(res, { base32, otpauthurl }, 200);
+  } catch (err) {}
+});
 
-      if (!validToken) {
-        return resCall({ status: "fail", message }, 401);
-      }
-
-      return resCall({ otp_valid: true }, 200);
-    } catch (err) {
-      resCall({ status: "error", message: err.message }, 500);
+const VerifyOTP = catchAsync(async (req: Request, res: Response) => {
+  try {
+    const { userid, token } = req.body;
+    const user = await prisma.user.findUnique({ where: { id: userid } });
+    const message = "Token is invalid or user doesn't exist";
+    if (!user) {
+      return resCall({ status: "fail", message }, 401);
     }
-  }
-);
 
-export { RegisterUser, LoginUser, GenerateOTP, VerifyOTP };
+    const verified = totp.verify({
+      secret: user.otp_base32!,
+      encoding: "base32",
+      token,
+    });
+
+    if (!verified) {
+      return resCall({ status: "fail", message }, 401);
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userid },
+      data: {
+        otp_enabled: true,
+        otp_verified: true,
+      },
+    });
+
+    resCall(
+      {
+        otp_verified: true,
+        user: {
+          id: updatedUser.id,
+          name: updatedUser.name,
+          email: updatedUser.email,
+          otp_enabled: updatedUser.otp_enabled,
+        },
+      },
+      200
+    );
+  } catch (err) {
+    resCall({ status: "error", message: err.message }, 500);
+  }
+});
+
+const ValidateOTP = catchAsync(async (req: Request, res: Response) => {
+  try {
+    const { userid, token } = req.body;
+    const user = await prisma.user.findUnique({ where: { id: userid } });
+
+    const message = "Token is invalid or user doesn't exist";
+    if (!user) {
+      return resCall({ status: "fail", message }, 401);
+    }
+
+    const validToken = totp.verify({
+      secret: user?.otp_base32 as string,
+      token,
+      window: 1,
+    });
+
+    if (!validToken) {
+      return resCall({ status: "fail", message }, 401);
+    }
+
+    return resCall({ otp_valid: true }, 200);
+  } catch (err) {
+    resCall({ status: "error", message: err.message }, 500);
+  }
+});
+
+const DisableOTP = catchAsync(async (req: Request, res: Request) => {
+  try {
+    const { userid } = req.body;
+
+    const user = await prisma.user.findUnique({ where: { id: userid } });
+    if (!user) {
+      return resCall({ status: "fail", message: "User doesn't exist" }, 401);
+    }
+
+    const updateUser = await prisma.user.update({
+      where: { id: userid },
+      data: {
+        otp_enabled: false,
+      },
+    });
+
+    resCall(
+      {
+        otp_disabled: true,
+        user: {
+          id: updateUser.id,
+          name: updateUser.name,
+          email: updateUser.email,
+          otp_enabled: updateUser.otp_enabled,
+        },
+      },
+      200
+    );
+  } catch (err) {
+    resCall({ status: "error", message: err.message }, 500);
+  }
+});
+
+export {
+  RegisterUser,
+  LoginUser,
+  GenerateOTP,
+  VerifyOTP,
+  ValidateOTP,
+  DisableOTP,
+};
